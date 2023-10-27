378712cbf0f4cb0fc4234246a976b067d7ca4f5d
dimka <ddd@ddd>
2023-10-27 17:09:28 +0300

    Insert provisioner for move Dockerfile to Build instance.

--- Buildwithtf.tf
+++ Buildwithtf.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+#Configure connection to my yandex.cloud
 provider "yandex" {
   token                    = ""
   cloud_id                 = "b1g076oa23iiut56kpqh"
@@ -18,12 +18,12 @@ resource "yandex_compute_instance" "test" {
   name        = "test"
   platform_id = "standard-v1"
   zone        = "ru-central1-a"
-
+#Chose count core and ram
   resources {
     cores  = 2
     memory = 4
   }
-
+#Chose ubuntu 20.04 (if I want to select another OC I can do in bash 'yc compute image list --folder-id standard-images | grep ubuntu' for exemple
   boot_disk {
     initialize_params {
       image_id = "fd8ciuqfa001h8s9sa7i"
@@ -33,9 +33,13 @@ resource "yandex_compute_instance" "test" {
   network_interface {
     subnet_id = "e9b6m0jmtruhhm3r4bdj"
   }
-
+#Indicate the path to the ssh key
   metadata = {
     ssh-keys = "ubuntu:${file("/home/dmitry/test/Lesson14Terraform/test.pub")}"
+  }
+   provisioner "file" {
+    source      = "/home/dmitry/test/Lesson14Terraform/Dockerfile"
+    destination = "/Home/Dockerfile"
   }
 }
 
