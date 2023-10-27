74a0c5d5466227943b8a29043976dcb82c5c71f9
dimka <ddd@ddd>
2023-10-27 16:07:45 +0300

    Initial commit

---
+++ Buildwithtf.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.100.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token                    = ""
+  service_account_key_file = "path_to_service_account_key_file"
+  cloud_id                 = "b1g076oa23iiut56kpqh"
+  folder_id                = "b1gs7vib6tce4ocj9pnr"
+  zone                     = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "test" {
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "image_id"
+    }
+  }
+
+  network_interface {
+    subnet_id = "default"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
